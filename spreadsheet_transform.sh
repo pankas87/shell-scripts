31bc18b65dc5059906876122b592a98b928ff27a
Leonardo Graterol <pankas87@gmail.com>
2015-08-07 15:12:57 -0400

    Spreadsheet transform

---
+++ spreadsheet_transform.sh
@@ -0,0 +1,41 @@
+# Transform all the spreadsheets in a folder from one format [XLS|XLSX|CSV] to the other two
+
+# It depends on Gnumeric package being installed in your system
+  # http://www.gnumeric.org/
+
+# It receives one parameter:
+  # folder
+FOLDER=$1
+
+cd "$FOLDER"
+
+for f in *.$2
+do
+  filename=$(basename "$f")
+  extension="${filename##*.}"
+  filename="${filename%.*}"
+
+  if [ $extension = "xlsx" ]; then
+    # Transform to CSV
+    ssconvert $f $filename".csv"
+
+    # Transform to XLS
+    ssconvert $f $filename".xls"
+  fi
+
+  if [ $extension = "xls" ]; then
+    # Transform to CSV
+    ssconvert $f $filename".csv"
+
+    # Transform to XLSX
+    ssconvert $f $filename".xls"
+  fi
+
+  if [ $extension = "csv" ]; then
+    # Transform to XLSX
+    ssconvert $f $filename".csv"
+
+    # Transform to XLS
+    ssconvert $f $filename".xls"
+  fi
+done
